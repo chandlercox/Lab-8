3a19d50d73477bfaf8f3550d328b6206b34f2f28
Joseph Miller <jmm1@iastate.edu>
2020-04-03 12:28:23 -0500

    lab files

---
+++ servo.c
@@ -0,0 +1,8 @@
+/*
+ * servo.c
+ *
+ *  Created on: Apr 3, 2020
+ *      Author: joemm
+ */
+
+
---
+++ servo.h
@@ -0,0 +1,13 @@
+/*
+ * servo.h
+ *
+ *  Created on: Apr 3, 2020
+ *      Author: joemm
+ */
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+
+
+#endif /* SERVO_H_ */
